32785cbb15350e93128a320cc81f24136cb304f6
Aaron Ruan <i@ar212.com>
2025-06-19 17:29:06 +0800

    bug fix 2

--- backend/src/main/java/com/aaron212/onlinelibrarymanagement/backend/BackendApplication.java
+++ backend/src/main/java/com/aaron212/onlinelibrarymanagement/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.aaron212.onlinelibrarymanagement.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class BackendApplication {
 
     public static void main(String[] args) {
