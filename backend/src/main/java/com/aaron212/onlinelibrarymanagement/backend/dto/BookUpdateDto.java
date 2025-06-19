c8fb74f313b83a3d0c2653f2f685e53840073359
Aaron Ruan <i@ar212.com>
2025-06-19 20:33:20 +0800

    dto validation

--- backend/src/main/java/com/aaron212/onlinelibrarymanagement/backend/dto/BookUpdateDto.java
+++ backend/src/main/java/com/aaron212/onlinelibrarymanagement/backend/dto/BookUpdateDto.java
@@ -1,7 +1,12 @@
 package com.aaron212.onlinelibrarymanagement.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record BookUpdateDto(
+        @NotBlank(message = "Title is required")
         String title,
+        @NotBlank(message = "Index category is required")
         String indexCategory,
+        @NotBlank(message = "Location is required")
         String location
 ) {}
